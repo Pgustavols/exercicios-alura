118c89073fbc3f63b2db32f0b7bbb824cb4ed0df
Paulo Gustavo <paulolisboa1000@gmail.com>
2026-09-10 18:04:43 -0300

    fix: adiciona variavel no sout

--- src/variaveis/ConversaoDeTipos.java
+++ src/variaveis/ConversaoDeTipos.java
@@ -2,6 +2,12 @@ package variaveis;
 
 public class ConversaoDeTipos {
     public static void main(String[] args) {
+        double valorDouble = 19.5;
 
+        int valorInt = (int) valorDouble;
+
+        System.out.println(valorInt);
+
+        System.out.println("O valor inteiro do produto é: "+valorInt);
     }
 }
