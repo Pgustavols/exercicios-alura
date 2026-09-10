a001dd2cf61e5245d0b147b22335d2c644e27379
Paulo Gustavo <paulolisboa1000@gmail.com>
2026-09-10 17:18:00 -0300

    feat: adiciona declaracao variaveis

---
+++ src/variaveis/DeclaracaoVariaveis.java
@@ -0,0 +1,15 @@
+package variaveis;
+
+public class DeclaracaoVariaveis {
+    public static void main(String[] args) {
+        String nome = "Paulo";
+        int idade = 22;
+        double altura = 1.74;
+        boolean ehEstudante = true;
+
+        System.out.println("Nome: "+nome);
+        System.out.println("Idade: "+idade);
+        System.out.println("Altura: "+altura);
+        System.out.println("É estudante: "+ehEstudante);
+    }
+}
