2d180dae893bad264c1bbfdd3684721ebf3dfaeb
Paulo Gustavo <paulolisboa1000@gmail.com>
2026-09-11 21:25:18 -0300

    adicionando classicacao por categoria e conversor de temperatura

--- src/variaveis/ClassificacaoPorCategoria.java
+++ src/variaveis/ClassificacaoPorCategoria.java
@@ -1,2 +1,23 @@
-package variaveis;public class ClassificacaoPorCategoria {
+package variaveis;
+
+import java.math.BigDecimal;
+
+public class ClassificacaoPorCategoria {
+    public static void main(String[] args) {
+        BigDecimal preco = new BigDecimal("50.00");
+
+        String categoria;
+
+        if(preco.compareTo(new BigDecimal("50.00")) <= 0){
+            categoria = "Econômico";
+        } else if (preco.compareTo(new BigDecimal("50.00")) > 0 && preco.compareTo(new BigDecimal("200.00")) <= 0) {
+            categoria = "Intermediário";
+        }else{
+            categoria = "Premium";
+        }
+
+        System.out.println("Categoria do produto: "+categoria);
+
+    }
+
 }
--- src/variaveis/ConversorDeTemperatura.java
+++ src/variaveis/ConversorDeTemperatura.java
@@ -1,2 +1,10 @@
-package variaveis;public class ConversorDeTemperatura {
+package variaveis;
+
+public class ConversorDeTemperatura {
+    public static void main(String[] args) {
+        int celsius = 20;
+        double fahrenheit = ((double) (celsius * 9) / 5) + 32;
+
+        System.out.println(celsius+"ºC equivalem a "+String.format("%.2f", fahrenheit)+"ºF");
+    }
 }
