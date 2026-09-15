e3a14f636d2b25c91ba68e5fc4a80c3df0fc43fb
Paulo Gustavo <paulolisboa1000@gmail.com>
2026-09-14 21:54:32 -0300

    feat: adiciona verifica aprovacao

--- src/condicionais/VerificaAprovacao.java
+++ src/condicionais/VerificaAprovacao.java
@@ -1,2 +1,23 @@
-package condicionais;public class VerificaAprovacao {
+package condicionais;
+
+import java.util.Scanner;
+
+public class VerificaAprovacao {
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)){
+            System.out.print("Informe a média do aluno: ");
+            double media = sc.nextDouble();
+            String status;
+
+            if(media >= 7){
+                status = "aprovado";
+            }else if(media >= 5 && media <= 6.9){
+                status = "recuperação";
+            }else{
+                status = "reprovado";
+            }
+
+            System.out.println("O estudante teve média "+String.format("%.1f", media) + ". Status: "+status);
+        }
+    }
 }
