3bdb2e1013fc6539bca7927d94836973bd8688a6
Paulo Gustavo <paulolisboa1000@gmail.com>
2026-09-19 11:59:41 -0300

    docs: adiciona o enunciado do exercicio intervalo

--- src/condicionais/Intervalo.java
+++ src/condicionais/Intervalo.java
@@ -1,6 +1,12 @@
 package condicionais;
 
 import java.util.Scanner;
+
+/*
+Emerson trabalha em um banco e precisa verificar se um número digitado pelo cliente está dentro da faixa permitida de valores para um empréstimo,
+que vai de 1000 a 5000 reais.
+Crie um programa que receba um valor e exiba se ele está dentro do intervalo permitido ou não.
+* */
 public class Intervalo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
