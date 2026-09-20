118382ca97acdbf694b2958d76a24c1f796d786c
Paulo Gustavo <paulolisboa1000@gmail.com>
2026-09-20 20:45:37 -0300

    feat: adiciona soma numeros

---
+++ src/repeticao/SomaNumeros.java
@@ -0,0 +1,14 @@
+package repeticao;
+
+public class SomaNumeros {
+    public static void main(String[] args) {
+        int[] valores = {10, 20, 30, 40, 50};
+        int soma = 0;
+
+        for(int i = 0; i < valores.length; i++){
+            soma += valores[i];
+        }
+
+        System.out.println("A soma total das receitas é: "+soma);
+    }
+}
